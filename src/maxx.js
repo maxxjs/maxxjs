1dcc4f0e611f871b118c63d65cc907141be9bca4
maxxjs <90232821+maxxjs@users.noreply.github.com>
2021-09-06 21:54:44 -0600

    Update maxx.js

--- src/maxx.js
+++ src/maxx.js
@@ -3,13 +3,13 @@ window.cpn = [];
 function CreateComponent(dict) {
   let type = dict["type"]
   if (type == "text-block") {
-   let text = dict["text"];
+    let text = dict["text"];
     let onclick = dict["onclick"];
     let name = dict["name"]
     let e = document.createElement('p')
     e.innerText = text;
     e.addEventListener('click', onclick)
-    window.cpn.push({recognize: `((${name}))`, assocElement:e})
+    window.cpn.push({recognize: `[[${name}]]`, assocElement:e})
   }
 }
   
